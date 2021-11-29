48fe27f56ecfa8220e79b1b5fe2a7c271e94d8ca
Florian Briegel <wasndas@zone42.de>
2021-11-29 17:02:38 +0100

    added test for fiber angles

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,25 +10,6 @@ underlying directories. See https://docs.pytest.org/en/2.7.3/plugins.html for
 more information.
 """
 
-import asyncio
-import uuid
 
 import pytest
-
-from proto.actor.actor import ProtoActor
-
-@pytest.fixture(scope="session")
-def event_loop():
-    return asyncio.get_event_loop()
-
-
-@pytest.fixture(scope="session")
-async def proto_test_actor(event_loop):
-
-    actor = ProtoActor(name=f"proto_test-{uuid.uuid4().hex[:8]}")
-    await actor.start()
-   
-    yield actor
-
-    await actor.stop()
 
