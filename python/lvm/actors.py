972d829b8ec2f41c54c25173032ef61406f69805
Florian Briegel <wasndas@zone42.de>
2022-06-22 00:21:20 -0700

    simplified lvm actors

--- python/lvm/actors.py
+++ python/lvm/actors.py
@@ -5,42 +5,18 @@
 # @Filename: lvm_actors.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
-import asyncio
-import sys
-import uuid
+from cluplus.proxy import Proxy
 
-from logging import DEBUG
-
-from sdsstools.logger import StreamFormatter  
-from sdsstools import get_logger, read_yaml_file
-from sdsstools.logger import SDSSLogger
-
-from clu import AMQPClient, CommandStatus
-
-from cluplus.proxy import Proxy, invoke, unpack
-
-lvm_amqpc = AMQPClient(name=f"{sys.argv[0]}.proxy-{uuid.uuid4().hex[:8]}")
-logger = lvm_amqpc.log
 
 class lvm:
-    def execute(coro, verbose=None):
-        async def start(coro):
-            await coro.start()
-
-        if verbose:
-           logger.sh.setLevel(DEBUG)
-           #logger.sh.formatter = StreamFormatter(fmt='%(asctime)s %(name)s %(levelname)s %(filename)s:%(lineno)d: \033[1m%(message)s\033[21m') 
-        return lvm_amqpc.loop.run_until_complete(coro)
-
     class sci:
-        foc = Proxy(lvm_amqpc, "lvm.sci.foc")
-        km = Proxy(lvm_amqpc, "lvm.sci.km")
-        pwi = Proxy(lvm_amqpc, "lvm.sci.pwi")
-        agc = Proxy(lvm_amqpc, "lvm.sci.agcam")
-        ag = Proxy(lvm_amqpc, "lvm.sci.ag")
+        foc = Proxy( "lvm.sci.foc")
+        km = Proxy( "lvm.sci.km")
+        pwi = Proxy( "lvm.sci.pwi")
+        agc = Proxy( "lvm.sci.agcam")
+        ag = Proxy( "lvm.sci.ag")
 
         async def start():
-            await lvm_amqpc.start()
             rc = await asyncio.gather(
                 lvm.sci.foc.start(),
                 lvm.sci.km.start(),
@@ -49,19 +25,17 @@ class lvm:
                 lvm.sci.ag.start(),
                 return_exceptions=True
             )
-            logger.debug(str(rc))
             return lvm.sci
 
 
     class skye:
-        foc = Proxy(lvm_amqpc, "lvm.skye.foc")
-        km = Proxy(lvm_amqpc, "lvm.skye.km")
-        pwi = Proxy(lvm_amqpc, "lvm.skye.pwi")
-        agc = Proxy(lvm_amqpc, "lvm.skye.agcam")
-        ag = Proxy(lvm_amqpc, "lvm.skye.ag")
+        foc = Proxy( "lvm.skye.foc")
+        km = Proxy( "lvm.skye.km")
+        pwi = Proxy( "lvm.skye.pwi")
+        agc = Proxy( "lvm.skye.agcam")
+        ag = Proxy( "lvm.skye.ag")
         
         async def start():
-            await lvm_amqpc.start()
             await invoke(
                 lvm.skye.foc.start(),
                 lvm.skye.km.start(),
@@ -70,16 +44,15 @@ class lvm:
                 lvm.skye.ag.start(),
                 return_exceptions=True
             )
-            logger.debug(str(rc))
             return lvm.skye
 
 
     class skyw:
-        foc = Proxy(lvm_amqpc, "lvm.skyw.foc")
-        km = Proxy(lvm_amqpc, "lvm.skyw.km")
-        pwi = Proxy(lvm_amqpc, "lvm.skyw.pwi")
-        agc = Proxy(lvm_amqpc, "lvm.skyw.agcam")
-        ag = Proxy(lvm_amqpc, "lvm.skyw.ag")
+        foc = Proxy( "lvm.skyw.foc")
+        km = Proxy( "lvm.skyw.km")
+        pwi = Proxy( "lvm.skyw.pwi")
+        agc = Proxy( "lvm.skyw.agcam")
+        ag = Proxy( "lvm.skyw.ag")
         async def start():
             await lvm_amqpc.start()
             await invoke(
@@ -90,16 +63,15 @@ class lvm:
                 lvm.skyw.ag.start(),
                 return_exceptions=True
             )
-            logger.debug(str(rc))
             return lvm.skyw
 
 
     class spec:
-        foc = Proxy(lvm_amqpc, "lvm.spec.foc")
-        fibsel = Proxy(lvm_amqpc, "lvm.spec.fibsel")
-        pwi = Proxy(lvm_amqpc, "lvm.spec.pwi")
-        agc = Proxy(lvm_amqpc, "lvm.spec.agcam")
-        ag = Proxy(lvm_amqpc, "lvm.spec.ag")
+        foc = Proxy( "lvm.spec.foc")
+        fibsel = Proxy( "lvm.spec.fibsel")
+        pwi = Proxy( "lvm.spec.pwi")
+        agc = Proxy( "lvm.spec.agcam")
+        ag = Proxy( "lvm.spec.ag")
         async def start():
             await lvm_amqpc.start()
             await invoke(
@@ -110,21 +82,16 @@ class lvm:
                 lvm.spec.ag.start(),
                 return_exceptions=True
             )
-            logger.debug(str(rc))
             return lvm.spec
 
-    def from_string(subsys: str, amqpc = None):
-        if amqpc:
-            lvm_amqpc = amqpc
-            logger = lvm_amqpc.log
+    def from_string(subsys: str):
         if subsys == 'sci': 
             return lvm.sci.start()
         elif subsys == 'skye': 
-            return lvm.skye.sci.start()
+            return lvm.skye.start()
         elif subsys == 'skyw': 
-            return lvm.skyw.sci.start()
+            return lvm.skyw.start()
         elif subsys == 'spec': 
-            return lvm.spec.sci.start()
+            return lvm.spec.start()
         else: return None
-
 
