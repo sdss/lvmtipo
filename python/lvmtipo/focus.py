7619192e43b0638bf172347135309a2065bacdbd
Florian Briegel <wasndas@zone42.de>
2023-03-10 19:38:21 +0100

    split data focus2temp for 4 telescopes

--- python/lvmtipo/focus.py
+++ python/lvmtipo/focus.py
@@ -2,22 +2,46 @@
 #
 # @Author: Florian Briegel (briegel@mpia.de)
 # @Date: 2021-08-18
-# @Filename: lvmtipo/focus.py
+# @Filename: lvmtipo/temp2focus.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
 
 import numpy as np
 
-tempdata = np.array([
-    [8.98, 35.412],
-    [12.99, 38],
-    [17.7, 41.0],
-])
+# TODO: put real values from the fits header into here.
+# these values are from Heidelberg from science and to spec 2 has been added
+temp_data = {
+    "lvm.sci.foc":
+        np.array([
+            [8.98, 35.412],
+            [12.99, 38],
+            [17.7, 41.0],
+        ]),
+    "lvm.skye.foc":
+        np.array([
+            [8.98, 35.412],
+            [12.99, 38],
+            [17.7, 41.0],
+        ]),
+    "lvm.skyw.foc":
+        np.array([
+            [8.98, 35.412],
+            [12.99, 38],
+            [17.7, 41.0],
+        ]),
+    "lvm.spec.foc":
+        np.array([
+            [8.98, 37.412],
+            [12.99, 40],
+            [17.7, 43.0],
+        ]),
+}
 
-def temp2focus( temperature:float, polynomial:float = 2):
+def temp2focus(telescope:str, temperature:float,  polynomial:float = 2):
     """Gathering focus based on temperature"""
 
-    d = tempdata.T
+    # get the polynomial coefficients
+    d = temp_data[telescope].T
     foc_est = np.polyfit(d[0], d[1], polynomial)
 
     return np.polyval(foc_est, temperature)
